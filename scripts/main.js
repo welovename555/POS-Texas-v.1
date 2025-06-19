4af67b3242f33f54ddc681224ebfbcf48ecf3f54
welovename555 <75744119+welovename555@users.noreply.github.com>
2025-06-19 21:45:20 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,9 @@
 import { router } from './router/index.js';
-// ไม่ต้อง import supabase ที่นี่แล้ว เพราะแต่ละส่วนจะไปเรียกใช้ใน api ของตัวเอง
 
-// ฟังก์ชันหลักของแอป จะเหลือหน้าที่แค่เริ่มการทำงานของ Router
 function initializeApp() {
-  console.log('Router Initializing...');
-  // เริ่มต้นโดยการบอกให้ router ไปที่หน้า '/login'
-  router('/login');
+  console.log('App Initializing with Router...');
+  // เริ่มต้นการทำงานของ Router
+  router.init();
 }
 
 initializeApp();
